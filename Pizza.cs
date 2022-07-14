dc380d4c307dfeb022f166d809bdd57e01f69c0a
AlexViti <alexviti12@gmail.com>
2022-07-14 14:07:14 +0200

    Add project files.

---
+++ Pizza.cs
@@ -0,0 +1,31 @@
+﻿namespace la_mia_pizzeria_static
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+
+        public Pizza()
+        {
+            Name = "";
+            Description = "";
+            Price = 0;
+            ImageUrl = "";
+        }
+
+        public Pizza(string name, string description, decimal price, string imageUrl)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            ImageUrl = imageUrl;
+        }
+
+        public string GetPrice()
+        {
+            return $"{Price:C2}";
+        }
+    }
+}
